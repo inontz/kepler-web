c4b7bd1d199982d42ac4b3bdd6a0301e37d00e9d
inontz <inontz@icloud.com>
2023-04-25 04:41:38 +0700

    Fixxxxxx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Liff } from "@line/liff/exports";
 
@@ -18,9 +18,18 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
 
       <main className={styles.main}>
         <div className="flex justify-center ...">
-          <Image className="object-contain object-center h-96 w-96" src="/logo-no-background.svg" alt="Kepler" />
+          <img className="object-contain object-center h-96 w-96" src="/logo-no-background.svg" alt="Kepler" />
         </div>
-  
+        {liff && <p>LIFF init succeeded.</p>}
+        {liffError && (
+          <>
+            <p>LIFF init failed.</p>
+            <p>
+              <code>{liffError}</code>
+            </p>
+          </>
+        )}
+{/*   
         <h1 className="text-3xl font-bold underline"> Hello world! </h1>
         {liff && <p>LIFF init succeeded.</p>}
         {liffError && (
@@ -31,19 +40,19 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
             </p>
           </>
         )}
-        {/* <a
+        <a
           href="/profile"
         >
           Click To See My Profile
         </a> */}
         
-        <a
+        {/* <a
           href="https://developers.line.biz/ja/docs/liff/"
           target="_blank"
           rel="noreferrer"
         >
           LIFF Documentation
-        </a>
+        </a> */}
       </main>
        {/* <picture>
             <img className="object-none object-cente w-24 h-24" src="/logo-no-background.svg" alt="Kepler"></img>
